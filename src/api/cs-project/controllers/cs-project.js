a41ede4b2529bad2369f2595a98809abcb2e917e
Meshv Patel <meshv1823@gmail.com>
2023-08-12 19:39:48 +0530

    added  cs schema

---
+++ src/api/cs-project/controllers/cs-project.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cs-project controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cs-project.cs-project');
---
+++ src/api/cs-project/services/cs-project.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cs-project service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::cs-project.cs-project');
